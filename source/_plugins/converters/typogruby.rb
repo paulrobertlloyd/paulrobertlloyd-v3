e391fb2a7f65c6d550166c462b6d216adf18732b
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2014-09-27 01:47:57 +0100

    Tyopgruby/Krmpygs all seems to be working properly again

--- source/_plugins/converters/typogruby.rb
+++ source/_plugins/converters/typogruby.rb
@@ -5,8 +5,7 @@ module Jekyll
   module Converters
     class Markdown < Converter
       def convert(content)
-        # BUG: We can't use the broader improve() method as typogruby applies itself to the entire page, and we don't always want to supress widows
-        return Typogruby.amp(Typogruby.caps(Typogruby.smartypants(Kramdown::Document.new(content).to_html)))
+        return Typogruby.improve(Kramdown::Document.new(content).to_html)
       end
     end
   end
