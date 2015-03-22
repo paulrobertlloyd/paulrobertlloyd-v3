b78ec1d5f39455ce9bdb040be4544f87288ec974
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-03-22 15:26:47 +0000

    Critical CSS loading

--- source/assets/_javascripts/global.js
+++ source/assets/_javascripts/global.js
@@ -1,7 +1,4 @@
 //= require modules/page__location
 //= require modules/embed
 //= require modules/search
-
-//= require vendor/promis/promise
-//= require vendor/fontfaceobserver/fontfaceobserver
 //= require vendor/picturefill/dist/picturefill
--- source/assets/_javascripts/head.js
+++ source/assets/_javascripts/head.js
@@ -2,7 +2,11 @@
     'use strict';
 
     var enhance = {};
+    var fullCSSKey = 'fullcss';
+    var fullJSKey = 'fulljs';
 
+    // loadJS: Load a JS file asynchronously.
+    // https://github.com/filamentgroup/loadJS/
     function loadJS(src) {
         var ref = doc.getElementsByTagName('script')[0];
         var script = doc.createElement('script');
@@ -13,6 +17,38 @@
     }
     enhance.loadJS = loadJS;
 
+    // loadCSS: Load a CSS file asynchronously.
+    // https://github.com/filamentgroup/loadCSS/
+    function loadCSS(href, before, media) {
+        var link = doc.createElement('link');
+        var ref = before || doc.getElementsByTagName('script')[0];
+        var sheets = doc.styleSheets;
+        link.rel = 'stylesheet';
+        link.href = href;
+        link.media = 'only x';
+
+        ref.parentNode.insertBefore(link, ref);
+
+        function toggleMedia() {
+            var defined;
+            for (var i = 0; i < sheets.length; i = i + 1){
+                if (sheets[i].href && sheets[i].href.indexOf(href) > -1) {
+                    defined = true;
+                }
+            }
+            if (defined) {
+                link.media = media || 'all';
+            } else {
+                setTimeout(toggleMedia);
+            }
+        }
+
+        toggleMedia();
+        return link;
+    }
+    enhance.loadCSS = loadCSS;
+
+    // getMeta function: get a meta tag by name
     function getMeta(metaname) {
         var metas = doc.getElementsByTagName('meta');
         var meta;
@@ -26,6 +62,8 @@
     }
     enhance.getMeta = getMeta;
 
+    // Cookie function
+    // https://github.com/filamentgroup/cookie/
     function cookie(name, value, days) {
         var expires;
         if (value === undefined) {
@@ -51,13 +89,23 @@
     }
     enhance.cookie = cookie;
 
+    // Load CSS styles asynchronously
+    var fullCSS = getMeta(fullCSSKey);
+    if (fullCSS && !cookie(fullCSSKey)) {
+        loadCSS(fullCSS.content);
+        cookie(fullCSSKey, 'true', 7);
+    }
+
+    // Cut the mustard…
     if (!(doc.querySelector && win.innerWidth)) {
         return;
     }
 
+    // …and add class if user agent does.
     doc.documentElement.className += ' js-enhanced';
 
-    var fullJS = getMeta('fulljs');
+    // Load JavaScript asynchronously
+    var fullJS = getMeta(fullJSKey);
     if (fullJS){
         loadJS(fullJS.content);
     }
