11f0ebd02e22b055ec2f4eb35454078a7668af88
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-03-24 19:41:54 +0000

    Embed.js now works with pages containing multiple embeds

--- source/assets/_javascripts/modules/embed.js
+++ source/assets/_javascripts/modules/embed.js
@@ -1,26 +1,33 @@
 // Progressively enhance links to embedded content
 // Inspired by the code used on http://2014.dconstruct.org/
-// = require polyfills/classList
+//
+// Requires an element with the following markup:
+// <p data-embed-src="[embed iframe url]" data-embed-type="[embed type]">
+//     <a class="button" href="[embed url]">[Fallback button text]</a>
+// </p>
 (function (win, doc) {
     'use strict';
 
+    // Embed.rb generates a <p> element, so look for those first
     var p = doc.getElementsByTagName('p');
-    p.forEach(function(entry) {
-        console.log(entry);
-    });
+    for (var i = 0; i < p.length; i = i + 1) {
+        console.log(p[i]);
 
-    var embed = p.classList.contains('embed');
-    var src;
-    var type;
+        // If a <p> has the right attributes…
+        if (p[i].hasAttribute('data-embed-src')) {
+            var embed = p[i];
+            var src;
+            var type;
 
-    if (embed) {
-        if (win.innerWidth > 400) {
-            src = embed.getAttribute('data-embed-src');
-            type = embed.getAttribute('data-embed-type');
-            embed.className += ' embed--' + type;
-            embed.innerHTML = '<iframe class="embed__iframe" src="' + src + '" width="500" height="300" frameborder="0" allowfullscreen></iframe>';
-            embed.removeAttribute('data-embed-src');
-            embed.removeAttribute('data-embed-type');
+            // …and if the viewport is wider than 400px, embed!
+            if (win.innerWidth > 400) {
+                src = embed.getAttribute('data-embed-src');
+                type = embed.getAttribute('data-embed-type');
+                embed.className += ' embed--' + type;
+                embed.innerHTML = '<iframe class="embed__iframe" src="' + src + '" width="500" height="300" frameborder="0" allowfullscreen></iframe>';
+                embed.removeAttribute('data-embed-src');
+                embed.removeAttribute('data-embed-type');
+            }
         }
     }
 
