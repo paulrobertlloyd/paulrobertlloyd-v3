96ca7052c038e55fd09f16d7d7c53d90816987ff
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-03-24 03:16:22 +0000

    Better functioning archive search

--- source/assets/_javascripts/modules/search.js
+++ source/assets/_javascripts/modules/search.js
@@ -6,13 +6,13 @@
     if (doc.querySelector('.form--search')) {
         var searchForm = doc.querySelector('.form--search');
         var searchInput = doc.querySelector('.form--search .form__input');
-        var searchResults = doc.createElement('div');
+        var searchResults = doc.createElement('ol');
 
         searchForm.appendChild(searchResults);
         searchForm.setAttribute('action', '#search');
         searchForm.removeAttribute('method');
         searchInput.removeAttribute('name');
-        searchResults.setAttribute('class', 'search-results');
+        searchResults.setAttribute('class', 'list list--inline list--inline--separated list--results');
 
         [].forEach.call(doc.querySelectorAll('.form--search .form__submit, .form--search .form__hidden'),
         function (e) {
@@ -21,9 +21,9 @@
 
         SimpleJekyllSearch.init({
             searchInput: doc.querySelector('.form--search .form__input'),
-            resultsContainer: doc.querySelector('.search-results'),
-            template: '<span class="search-results__result">{type}: <a href="{url}">{title}</a></span>',
-            noResultsText: '<p>Nothing matched your query</p>',
+            resultsContainer: doc.querySelector('.list--results'),
+            searchResultTemplate: '<li class="list__item list__item--{type}"><a href="{url}">{title}</a></li>',
+            noResultsText: '<li>Nothing matched your query</li>',
             dataSource: '/archive/search.json',
             limit: 10,
         });
