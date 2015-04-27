f2d86ea1849915b3a5d0b657102f9442ee9f97a1
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-04-27 01:44:02 +0100

    Fix embeds, remove unneeded JS

--- source/assets/_javascripts/global.js
+++ source/assets/_javascripts/global.js
@@ -1,7 +1,6 @@
 // = require modules/banner
 // = require modules/embed
 // = require modules/fonts
-// = require modules/data-imgsrc
 // = require modules/page__location
 // = require modules/picture
 // = require modules/search
