2c8325467e1aaab0047272193e3a3c37d25a8f1d
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-06-01 14:49:59 +0100

    Fix submodules

--- source/assets/_javascripts/vendor/fontfaceobserver/src/main.js
+++
@@ -1,9 +0,0 @@
-goog.require('fontface.Observer');
-
-/**
- * @define {boolean} DEBUG
- */
-var DEBUG = true;
-
-window['FontFaceObserver'] = fontface.Observer;
-window['FontFaceObserver']['prototype']['check'] = fontface.Observer.prototype.check;
