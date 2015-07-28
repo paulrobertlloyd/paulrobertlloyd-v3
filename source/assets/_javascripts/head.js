ec962928ea4c001a48b83f06175ef5090c90e02a
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-07-28 15:12:35 +0100

    All head based JS should be in head.js

--- source/assets/_javascripts/head.js
+++ source/assets/_javascripts/head.js
@@ -116,3 +116,18 @@
     win.enhance = enhance;
 
 }(this, this.document));
+
+!(function (g, s, q, r, d) {
+    'use strict';
+
+    r = g[r] = g[r] || function () {
+        (r.q = r.q || []).push(arguments);
+    };
+    d = s.createElement(q);
+    q = s.getElementsByTagName(q)[0];
+    d.src = '//d1l6p2sc9645hc.cloudfront.net/tracker.js';
+    q.parentNode.
+    insertBefore(d, q);
+}(window, document, 'script', '_gs'));
+
+_gs('GSN-577964-G');
