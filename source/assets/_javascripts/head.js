2f757e8c23ba87e4ecea0262a38cf03cce721fc4
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-07-29 11:25:40 +0100

    Use HTTPS for analytics scripts, DNS prefetch

--- source/assets/_javascripts/head.js
+++ source/assets/_javascripts/head.js
@@ -125,7 +125,7 @@
     };
     d = s.createElement(q);
     q = s.getElementsByTagName(q)[0];
-    d.src = '//d1l6p2sc9645hc.cloudfront.net/tracker.js';
+    d.src = 'https://d1l6p2sc9645hc.cloudfront.net/tracker.js';
     q.parentNode.
     insertBefore(d, q);
 }(window, document, 'script', '_gs'));
