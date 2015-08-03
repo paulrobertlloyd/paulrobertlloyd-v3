3464ddde2e6e5390a5b16ee36a84fc534f004432
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-08-03 02:23:43 +0100

    Use correct webmention API endpoint

--- source/assets/_javascripts/global.js
+++ source/assets/_javascripts/global.js
@@ -10,7 +10,7 @@
     router.route(':year/:month/:slug', function () {
         new Webmentions({
             container: document.querySelector('#webmentions'),
-            endpoint: 'https://webmention.paulrobertlloyd.com',
+            endpoint: 'https://webmention.paulrobertlloyd.com/api/webmentions',
             params: {
                 'target': location.href.replace(location.hash, '')
             }
