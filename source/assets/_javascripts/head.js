4edfabdcd36c67129acc6dca7236401a9f2e4dea
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-08-11 01:33:24 +0100

    Remove GoSquared analytics

--- source/assets/_javascripts/head.js
+++ source/assets/_javascripts/head.js
@@ -116,18 +116,3 @@
     win.enhance = enhance;
 
 }(this, this.document));
-
-!(function (g, s, q, r, d) {
-    'use strict';
-
-    r = g[r] = g[r] || function () {
-        (r.q = r.q || []).push(arguments);
-    };
-    d = s.createElement(q);
-    q = s.getElementsByTagName(q)[0];
-    d.src = 'https://d1l6p2sc9645hc.cloudfront.net/tracker.js';
-    q.parentNode.
-    insertBefore(d, q);
-}(window, document, 'script', '_gs'));
-
-_gs('GSN-577964-G');
