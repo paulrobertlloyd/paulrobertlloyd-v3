8abf0d84ef9d08986b695cabf5e6da84cbe6fdc0
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2015-08-19 01:28:31 +0100

    Correct place for webmention template content

--- source/assets/_javascripts/modules/webmentions/likes.js
+++ source/assets/_javascripts/modules/webmentions/likes.js
@@ -34,7 +34,7 @@
 
             anchor.setAttribute('href', author.url[0]);
             avatar.setAttribute('alt', author.name[0]);
-            avatar.setAttribute('src', avatar.src + author.photo[0]);
+            avatar.setAttribute('src', 'https://images.paulrobertlloyd.com/unsafe/120x120/' + author.photo[0]);
             avatar.setAttribute('title', author.name[0] + ' likes this.');
 
             this.list.appendChild(template);
