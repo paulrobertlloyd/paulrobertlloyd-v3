971185641bc8691a6a48eb19ffb546357140ca89
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-01-29 16:46:07 +0000

    Update embed markup so more accessible when replaced with iframe

--- source/_assets/javascripts/modules/embed.js
+++ source/_assets/javascripts/modules/embed.js
@@ -2,33 +2,27 @@
 // Inspired by the code used on http://2014.dconstruct.org/
 //
 // Requires an element with the following markup:
-// <p data-embed-src="[embed iframe url]" data-embed-type="[embed type]">
-//     <a class="button" href="[embed url]">[Fallback button text]</a>
-// </p>
+// <div data-embed-src="[embed iframe url]" data-embed-type="[embed type]">
+//     <p><a class="button" href="[embed url]">[Fallback button text]</a></p>
+// </div>
 
 (function (win, doc) {
     'use strict';
 
-    // var breakpoint = win.getComputedStyle(doc.documentElement, ':after').getPropertyValue('content');
-    var p          = doc.getElementsByTagName('p'); // Embed.rb generates a <p> element, so look for those first
+    var div = doc.getElementsByTagName('div'); // Embed.rb generates a <div> element, so look for those first
 
-    for (var i = 0; i < p.length; i = i + 1) {
+    for (var i = 0; i < div.length; i = i + 1) {
 
-        // If a <p> has the right attributes…
-        if (p[i].hasAttribute('data-embed-src')) {
-            var embed = p[i];
-            var src;
-            var type;
+        // If a <div> has the right attributes…
+        if (div[i].hasAttribute('data-embed-src')) {
+            var embed = div[i];
+            var src = embed.getAttribute('data-embed-src');
+            var type = embed.getAttribute('data-embed-type');
 
-            // …and if the breakpint is not that of 's', embed!
-            // if (breakpoint.indexOf('s') == -1) {
-            src = embed.getAttribute('data-embed-src');
-            type = embed.getAttribute('data-embed-type');
             embed.className += ' c-embed--' + type;
             embed.innerHTML = '<iframe class="c-embed__iframe" src="' + src + '" width="500" height="300" frameborder="0" allowfullscreen></iframe>';
             embed.removeAttribute('data-embed-src');
             embed.removeAttribute('data-embed-type');
-            // }
         }
     }
 
