f4269f6e2a0a78719436945341e6adf1866589e4
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-01-29 16:53:20 +0000

    Add titles to iframes

--- source/_assets/javascripts/modules/embed.js
+++ source/_assets/javascripts/modules/embed.js
@@ -20,7 +20,7 @@
             var type = embed.getAttribute('data-embed-type');
 
             embed.className += ' c-embed--' + type;
-            embed.innerHTML = '<iframe class="c-embed__iframe" src="' + src + '" width="500" height="300" frameborder="0" allowfullscreen></iframe>';
+            embed.innerHTML = '<iframe class="c-embed__iframe" src="' + src + '" width="500" height="300" frameborder="0" title="Embedded media" allowfullscreen></iframe>';
             embed.removeAttribute('data-embed-src');
             embed.removeAttribute('data-embed-type');
         }
