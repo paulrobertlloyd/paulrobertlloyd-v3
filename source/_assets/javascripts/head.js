9fcbd31f787ae2f19916ac66ff95a81c69b43718
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-07-21 00:03:16 +0100

    4 spaces => 2 spaces

--- source/_assets/javascripts/head.js
+++ source/_assets/javascripts/head.js
@@ -1,118 +1,118 @@
 (function (win, doc, undefined) {
-    'use strict';
+  'use strict';
 
-    var enhance = {};
-    var fullCSSKey = 'fullcss';
-    var fullJSKey = 'fulljs';
+  var enhance = {};
+  var fullCSSKey = 'fullcss';
+  var fullJSKey = 'fulljs';
 
-    // loadJS: Load a JS file asynchronously.
-    // https://github.com/filamentgroup/loadJS/
-    function loadJS(src) {
-        var ref = doc.getElementsByTagName('script')[0];
-        var script = doc.createElement('script');
-        script.src = src;
-        script.async = true;
-        ref.parentNode.insertBefore(script, ref);
-        return script;
-    }
-    enhance.loadJS = loadJS;
+  // loadJS: Load a JS file asynchronously.
+  // https://github.com/filamentgroup/loadJS/
+  function loadJS(src) {
+    var ref = doc.getElementsByTagName('script')[0];
+    var script = doc.createElement('script');
+    script.src = src;
+    script.async = true;
+    ref.parentNode.insertBefore(script, ref);
+    return script;
+  }
+  enhance.loadJS = loadJS;
 
-    // loadCSS: Load a CSS file asynchronously.
-    // https://github.com/filamentgroup/loadCSS/
-    function loadCSS(href, before, media) {
-        var link = doc.createElement('link');
-        var ref = before || doc.getElementsByTagName('script')[0];
-        var sheets = window.document.styleSheets;
-        link.rel = 'stylesheet';
-        link.href = href;
-        link.media = 'only x';
+  // loadCSS: Load a CSS file asynchronously.
+  // https://github.com/filamentgroup/loadCSS/
+  function loadCSS(href, before, media) {
+    var link = doc.createElement('link');
+    var ref = before || doc.getElementsByTagName('script')[0];
+    var sheets = window.document.styleSheets;
+    link.rel = 'stylesheet';
+    link.href = href;
+    link.media = 'only x';
 
-        ref.parentNode.insertBefore(link, ref);
+    ref.parentNode.insertBefore(link, ref);
 
-        link.onloadcssdefined = function (callback) {
-            var defined;
-            for (var i = 0; i < sheets.length; i = i + 1) {
-                if (sheets[ i ].href && sheets[i].href.indexOf(href) > -1) {
-                    defined = true;
-                }
-            }
-            if (defined) {
-                callback();
-            } else {
-                setTimeout (function () {
-                    link.onloadcssdefined(callback);
-                });
-            }
-        };
-        link.onloadcssdefined(function () {
-            link.media = media || 'all';
+    link.onloadcssdefined = function (callback) {
+      var defined;
+      for (var i = 0; i < sheets.length; i = i + 1) {
+        if (sheets[ i ].href && sheets[i].href.indexOf(href) > -1) {
+          defined = true;
+        }
+      }
+      if (defined) {
+        callback();
+      } else {
+        setTimeout (function () {
+          link.onloadcssdefined(callback);
         });
-        return link;
+      }
+    };
+    link.onloadcssdefined(function () {
+      link.media = media || 'all';
+    });
+    return link;
+  }
+  enhance.loadCSS = loadCSS;
+
+  // getMeta function: get a meta tag by name
+  function getMeta(metaname) {
+    var metas = doc.getElementsByTagName('meta');
+    var meta;
+    for (var i = 0; i < metas.length; i = i + 1){
+      if (metas[i].name && metas[i].name === metaname){
+        meta = metas[ i ];
+        break;
+      }
     }
-    enhance.loadCSS = loadCSS;
+    return meta;
+  }
+  enhance.getMeta = getMeta;
 
-    // getMeta function: get a meta tag by name
-    function getMeta(metaname) {
-        var metas = doc.getElementsByTagName('meta');
-        var meta;
-        for (var i = 0; i < metas.length; i = i + 1){
-            if (metas[i].name && metas[i].name === metaname){
-                meta = metas[ i ];
-                break;
-            }
-        }
-        return meta;
+  // Cookie function
+  // https://github.com/filamentgroup/cookie/
+  function cookie(name, value, days) {
+    var expires;
+    if (value === undefined) {
+      var cookiestring = '; ' + doc.cookie;
+      var cookies = cookiestring.split('; ' + name + '=');
+      if (cookies.length === 2){
+        return cookies.pop().split(';').shift();
+      }
+      return null;
+    } else {
+      if (value === false) {
+        days = -1;
+      }
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toGMTString();
+      } else {
+        expires = '';
+      }
+      doc.cookie = name + '=' + value + expires + '; path=/';
     }
-    enhance.getMeta = getMeta;
+  }
+  enhance.cookie = cookie;
 
-    // Cookie function
-    // https://github.com/filamentgroup/cookie/
-    function cookie(name, value, days) {
-        var expires;
-        if (value === undefined) {
-            var cookiestring = '; ' + doc.cookie;
-            var cookies = cookiestring.split('; ' + name + '=');
-            if (cookies.length === 2){
-                return cookies.pop().split(';').shift();
-            }
-            return null;
-        } else {
-            if (value === false) {
-                days = -1;
-            }
-            if (days) {
-                var date = new Date();
-                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-                expires = '; expires=' + date.toGMTString();
-            } else {
-                expires = '';
-            }
-            doc.cookie = name + '=' + value + expires + '; path=/';
-        }
-    }
-    enhance.cookie = cookie;
+  // Load CSS styles asynchronously
+  var fullCSS = getMeta(fullCSSKey);
+  if (fullCSS && !cookie(fullCSSKey)) {
+    loadCSS(fullCSS.content);
+    cookie(fullCSSKey, 'true', 7);
+  }
 
-    // Load CSS styles asynchronously
-    var fullCSS = getMeta(fullCSSKey);
-    if (fullCSS && !cookie(fullCSSKey)) {
-        loadCSS(fullCSS.content);
-        cookie(fullCSSKey, 'true', 7);
-    }
+  // Cut the mustard…
+  if (!(doc.querySelector && win.innerWidth)) {
+    return;
+  }
 
-    // Cut the mustard…
-    if (!(doc.querySelector && win.innerWidth)) {
-        return;
-    }
+  // …and add class if user agent does.
+  doc.documentElement.className += ' js-enhanced';
 
-    // …and add class if user agent does.
-    doc.documentElement.className += ' js-enhanced';
+  // Load JavaScript asynchronously
+  var fullJS = getMeta(fullJSKey);
+  if (fullJS){
+    loadJS(fullJS.content);
+  }
 
-    // Load JavaScript asynchronously
-    var fullJS = getMeta(fullJSKey);
-    if (fullJS){
-        loadJS(fullJS.content);
-    }
-
-    win.enhance = enhance;
+  win.enhance = enhance;
 
 }(this, this.document));
--- source/_assets/javascripts/modules/embed.js
+++ source/_assets/javascripts/modules/embed.js
@@ -3,29 +3,29 @@
 //
 // Requires an element with the following markup:
 // <div data-embed-src="[embed iframe url]" data-embed-type="[embed type]">
-//     <p><a class="button" href="[embed url]">[Fallback button text]</a></p>
+//   <p><a class="button" href="[embed url]">[Fallback button text]</a></p>
 // </div>
 
 (function (win, doc) {
-    'use strict';
+  'use strict';
 
-    var div = doc.getElementsByTagName('div'); // Embed.rb generates a <div> element, so look for those first
+  var div = doc.getElementsByTagName('div'); // Embed.rb generates a <div> element, so look for those first
 
-    for (var i = 0; i < div.length; i = i + 1) {
+  for (var i = 0; i < div.length; i = i + 1) {
 
-        // If a <div> has the right attributes…
-        if (div[i].hasAttribute('data-embed-src')) {
-            var embed = div[i];
-            var type = embed.getAttribute('data-embed-type');
-            var src = embed.getAttribute('data-embed-src');
-            var title = embed.getAttribute('data-embed-title');
+    // If a <div> has the right attributes…
+    if (div[i].hasAttribute('data-embed-src')) {
+      var embed = div[i];
+      var type = embed.getAttribute('data-embed-type');
+      var src = embed.getAttribute('data-embed-src');
+      var title = embed.getAttribute('data-embed-title');
 
-            embed.className += ' c-embed--' + type;
-            embed.innerHTML = '<iframe class="c-embed__iframe" src="' + src + '" width="500" height="300" frameborder="0" title="' + title + '" allowfullscreen></iframe>';
-            embed.removeAttribute('data-embed-src');
-            embed.removeAttribute('data-embed-type');
-            embed.removeAttribute('data-embed-title');
-        }
+      embed.className += ' c-embed--' + type;
+      embed.innerHTML = '<iframe class="c-embed__iframe" src="' + src + '" width="500" height="300" frameborder="0" title="' + title + '" allowfullscreen></iframe>';
+      embed.removeAttribute('data-embed-src');
+      embed.removeAttribute('data-embed-type');
+      embed.removeAttribute('data-embed-title');
     }
+  }
 
 }(this, this.document));
